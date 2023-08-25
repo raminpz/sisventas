51f530e0b1409d43449d755047c3a4cdc0bce6f0
Ramiro Nuñez <raminp.1012@gmail>
2023-08-24 20:10:55 -0500

    Initial commit

---
+++ src/main/java/com/softrami/sisventas/app/SisventasApplication.java
@@ -0,0 +1,13 @@
+package com.softrami.sisventas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisventasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisventasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softrami/sisventas/app/SisventasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softrami.sisventas.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisventasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
